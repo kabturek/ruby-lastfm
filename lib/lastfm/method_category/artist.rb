b87de6105215cbd00989ef8bd56a1e0b26540325
Kyle Fleming <kyle@rexly.com>
2011-08-29 15:00:50 -0700

    add artist.get_top_tags

--- lib/lastfm/method_category/artist.rb
+++ lib/lastfm/method_category/artist.rb
@@ -7,6 +7,9 @@ class Lastfm
       regular_method :get_events, [:artist], [] do |response|
         response.xml['events']['event']
       end
+      regular_method :get_top_tags, [:artist], [] do |response|
+        response.xml['toptags']['tag']
+      end
     end
   end
 end
