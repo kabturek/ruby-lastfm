b24e0bffb1d209d4b0d92fc7542e34ef13300d76
koshikawa <koshikawa@ppworks.jp>
2011-09-12 06:01:23 +0900

    artist image support limit parameter

--- lib/lastfm/method_category/artist.rb
+++ lib/lastfm/method_category/artist.rb
@@ -10,7 +10,7 @@ class Lastfm
       regular_method :get_top_tags, [:artist], [] do |response|
         response.xml['toptags']['tag']
       end
-      regular_method :get_images, [:artist], [] do |response|
+      regular_method :get_images, [:artist], [[:limit, nil], [:page, nil]] do |response|
         response.xml['images']['image']
       end
       regular_method :search, [:artist], [[:limit, nil], [:page, nil]] do |response|
